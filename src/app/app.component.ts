c62e435ac6231ba1f4eceab2afe5a8c725111360
lucasbennertz <lucasgcbennertz@gmail.com>
2025-04-10 15:08:32 -0300

    inserção de aba about-me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './template/header/header.component';
 import { FooterComponent } from './template/footer/footer.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutMeComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
